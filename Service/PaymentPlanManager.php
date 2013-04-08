d6531886e6feefbaaae4d4a1876d8e9b11d0e883
Stuart Chapman <github@stuart-chapman.com>
2013-04-08 12:35:04 +0000

    Alternative versions of payment plans available in PaymentPlanManager.

--- Service/PaymentPlanManager.php
+++ Service/PaymentPlanManager.php
@@ -9,23 +9,26 @@ use Ice\MercuryClientBundle\PaymentPlan\ResidentialRegistrationFee;
 class PaymentPlanManager
 {
     /**
-     * Returns the payment plan relating to the payment plan code.
+     * Returns the payment plan relating to the payment plan code and, optionally, version.
      *
-     * @param string    $paymentPlanCode
+     * @param string $paymentPlanCode
+     * @param string $version Optional payment plan version
      *
      * @throws \Ice\MercuryClientBundle\Exception\InvalidPaymentPlanException
      * @return AbstractPaymentPlan
      */
-    public function getPaymentPlan($paymentPlanCode)
+    public function getPaymentPlan($paymentPlanCode, $version = '1')
     {
         $mapper = array(
-            'residentialRegistrationFee' => new ResidentialRegistrationFee(),
+            'residentialRegistrationFee' => array(
+                '1' => new ResidentialRegistrationFee(),
+            ),
         );
 
-        if (!array_key_exists($paymentPlanCode, $mapper)) {
+        if (!array_key_exists($paymentPlanCode, $mapper) || !array_key_exists($version, $mapper[$paymentPlanCode])) {
             throw new InvalidPaymentPlanException;
         }
 
-        return $mapper[$paymentPlanCode];
+        return $mapper[$paymentPlanCode][$version];
     }
 }
