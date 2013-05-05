26b13af4e8d4adbef67a5606d2593e13e2090040
Rob Hogan <rh389@cam.ac.uk>
2013-05-05 15:50:13 +0000

    Added ability to run tests against Mercury's test environment. Added some integration tests

---
+++ Tests/Util/WebTestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ice\MercuryClientBundle\Tests\Util;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase AS BaseWebTestCase;
+
+class WebTestCase extends BaseWebTestCase
+{
+    /**
+     * @param array $options
+     * @return TestKernel|\Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    public static function createKernel(array $options = array())
+    {
+        return new TestKernel('test', false);
+    }
+}
