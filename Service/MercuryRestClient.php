4ee59abadd677e04fd6be816de1dc6df8564f0af
Stuart Chapman <github@stuart-chapman.com>
2013-05-10 09:02:56 +0000

    Add authentication to client

--- Service/MercuryRestClient.php
+++ Service/MercuryRestClient.php
@@ -7,13 +7,22 @@ class MercuryRestClient extends Client
 {
 
     /**
-     * @param \Guzzle\Service\Client $guzzleClient
+     * @param string $baseUrl
+     * @param string $username
+     * @param string $password
+     * @param null   $config
+     *
+     * @internal param \Guzzle\Service\Client $guzzleClient
      */
-    public function __construct($baseUrl = '', $config = null){
+    public function __construct($baseUrl = '', $username, $password, $config = null){
         parent::__construct($baseUrl, $config);
+        $this->setConfig(array(
+            'curl.options' => array(
+                'CURLOPT_USERPWD' => sprintf("%s:%s", $username, $password),
+            ),
+        ));
         $this->setDefaultHeaders(array(
-            'Accept'=> 'application/json',
-            'Content-Type'=>'application/json'
+            'Accept' => 'application/json',
         ));
     }
 }
