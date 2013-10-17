a8b49770ac853edfb08079f4a15492b4ccf22d35
Rob Hogan <rh389@cam.ac.uk>
2013-10-17 15:02:59 +0000

    Pick up additional Authorisation properties

--- Entity/Authorisation.php
+++ Entity/Authorisation.php
@@ -28,6 +28,30 @@ class Authorisation
     private $transactionReference;
 
     /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $maskedPan;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $cardholderFullName;
+
+    /**
+     * @var \DateTime
+     * @JMS\Type("DateTime")
+     */
+    private $expiryDate;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $paymentType;
+
+    /**
      * @param string $iceId
      * @return Authorisation
      */
@@ -79,5 +103,77 @@ class Authorisation
     public function getTransactionReference()
     {
         return $this->transactionReference;
+    }
+
+    /**
+     * @param string $cardholderFullName
+     * @return Authorisation
+     */
+    public function setCardholderFullName($cardholderFullName)
+    {
+        $this->cardholderFullName = $cardholderFullName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardholderFullName()
+    {
+        return $this->cardholderFullName;
+    }
+
+    /**
+     * @param \DateTime $expiryDate
+     * @return Authorisation
+     */
+    public function setExpiryDate($expiryDate)
+    {
+        $this->expiryDate = $expiryDate;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getExpiryDate()
+    {
+        return $this->expiryDate;
+    }
+
+    /**
+     * @param string $maskedPan
+     * @return Authorisation
+     */
+    public function setMaskedPan($maskedPan)
+    {
+        $this->maskedPan = $maskedPan;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMaskedPan()
+    {
+        return $this->maskedPan;
+    }
+
+    /**
+     * @param string $paymentType
+     * @return Authorisation
+     */
+    public function setPaymentType($paymentType)
+    {
+        $this->paymentType = $paymentType;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentType()
+    {
+        return $this->paymentType;
     }
 }
